23fab22824d1c5f754d75198b090039db9074f4b
One-Punch-Superman <764213669@qq.com>
2022-04-12 22:17:40 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,7 @@ const routes: RouteRecordRaw[] = [
         component: Layout,
         children: [
             {
-                path: '/',
+                path: '',
                 name: 'Login',
                 component: Home
             },
