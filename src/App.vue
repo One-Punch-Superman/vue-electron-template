aaa83f4de0b50bfab01a99d99c1226b3f346de17
One-Punch-Superman <764213669@qq.com>
2022-04-12 09:00:20 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <router-view></router-view>
+    <el-config-provider :locale="zhCn">
+        <router-view></router-view>
+    </el-config-provider>
 </template>
+
+<script lang="ts" setup>
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+</script>
