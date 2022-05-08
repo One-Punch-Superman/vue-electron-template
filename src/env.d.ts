e8d73060ae04793faab5df63a3f9b784750f373c
One-Punch-Superman <764213669@qq.com>
2022-05-08 18:28:43 +0800

    init

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,7 @@ declare module '*.vue' {
     export default component;
 }
 
+declare module 'nprogress';
 declare module '@kangc/v-md-editor';
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js';
 declare module '@kangc/v-md-editor/lib/plugins/copy-code/index';
