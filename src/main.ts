67fb3f21e571e3f17b365cf438973eb6098eb1c9
One-Punch-Superman <764213669@qq.com>
2022-07-06 00:24:31 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import { createApp } from 'vue';
-import router from './router';
-import store from './store';
-import App from './App.vue';
-
-import 'normalize.css';
 
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
@@ -18,9 +13,16 @@ import Prism from 'prismjs';
 
 VMdEditor.use(vuepressTheme, { Prism }).use(createCopyCodePlugin());
 
+import router from './router';
+import store from './store';
+import '@/style/index.scss';
+import App from './App.vue';
+
 const app = createApp(App);
+
 app.use(router);
 app.use(store);
 app.use(VMdEditor);
 app.use(ElementPlus);
+
 app.mount('#app');
