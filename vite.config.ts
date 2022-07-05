a3f7a9aef476e717712452d9e630a83fe8de71a6
One-Punch-Superman <764213669@qq.com>
2022-07-05 23:57:56 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,43 +5,43 @@ import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path';
 
 const PROXY = {
-    target: 'https://www.huawei.com/',
-    secure: false,
-    ws: true,
-    changeOrigin: true
+  target: 'https://www.huawei.com/',
+  secure: false,
+  ws: true,
+  changeOrigin: true
 };
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            imports: ['vue', 'vue-router'],
-            eslintrc: {
-                enabled: false, // Default `false`
-                filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-                globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-            }
-        }),
-        prismjs({
-            languages: 'all'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 8080,
-        open: true,
-        proxy: {
-            rest: PROXY
-        },
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-        }
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    }),
+    prismjs({
+      languages: 'all'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
+    proxy: {
+      rest: PROXY
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  }
 });
